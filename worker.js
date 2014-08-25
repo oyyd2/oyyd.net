10ff5d7212d5e8c4139a5ee86c33b74874e3bab4
oyyd <oyydoibh@gmail.com>
2014-08-25 15:07:42 +0800

    Change to app.oyyd.net

--- worker.js
+++ worker.js
@@ -10,16 +10,25 @@ var express = require('express'),
 //configuration
 app.engine('jade',require('jade').__express);
 app.set('views','views/');
-// app.use(express.logger());
-app.use(lessMiddleware({
-    dest:__dirname+'/static/css',
-    src:__dirname+'/less',
-    prefix:'/static/css',
-    compress:true
+
+//Use and config less-middleware.
+// app.use(lessMiddleware({
+//     dest:__dirname+'/static/css',
+//     src:__dirname+'/less',
+//     prefix:'/static/css',
+//     compress:true
+// }));
+//Import and config less module.
+app.use('/static/css',lessMiddleware(__dirname + '/less',{
+    dest:__dirname+'/static/css'
 }));
-app.use(express.bodyParser());
+
+//Express config.
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use('/static',express.static(__dirname+'/static'));
+app.use('/client',express.static(__dirname+'/client'));
 app.use('/js',express.static(__dirname+'/js'));
 
 //Init route    
